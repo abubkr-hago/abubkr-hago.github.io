b60970b8924732ec7a80ece4ba8fd4507aa20db7
abubkr-hago <24502430+abubkr-hago@users.noreply.github.com>
2024-01-08 03:57:14 +0000

    Deploying to gh-pages from @ abubkr-hago/abubkr-hago.github.io@7792682b083669fc289991647043fbdb39e95ad2 🚀

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {
-    images: {unoptimized: true},experimental: {images: {unoptimized: true}},basePath: "",distDir: 'build',
-    poweredByHeader: false,
-    output: 'export'
-}
-
-module.exports = nextConfig
